427f5310cb2180f2184f8cb17843d63ba101e3f4
Nionik <Patrycja.pl.opalacz@gmail.com>
2025-03-01 10:32:04 +0100

    Dodaj encję Student

---
+++ Domain/Student.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        public Student()
+        {
+            Enrollments = new List<Enrollment>();
+        }
+    }
+} 
