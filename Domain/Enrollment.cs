c0ce8493bdfae0a68d825f96ee79d7447a36b6e7
Nionik <Patrycja.pl.opalacz@gmail.com>
2025-03-05 12:00:00 +0100

    Dodaj encję Enrollment

---
+++ Domain/Enrollment.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public Enrollment() {}
+    }
+} 
